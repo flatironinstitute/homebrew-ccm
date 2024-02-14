ce361efa8346f8e1ae6c1cc27a30b7771ebcbe79
Dan Fortunato <dan.fortunato@gmail.com>
2024-02-14 00:15:45 -0500

    Add fmm3d formula
    Closes #1.
    
    Signed-off-by: Dan Fortunato <dan.fortunato@gmail.com>

---
+++ Formula/fmm3d.rb
@@ -0,0 +1,27 @@
+class Fmm3d < Formula
+  desc "Fast multipole methods in three dimensions"
+  homepage "https://fmm3d.readthedocs.io"
+  url "https://github.com/flatironinstitute/FMM3D/archive/refs/tags/v1.0.2.tar.gz"
+  sha256 "e29a1ebe485d2431d4e330334b2853bc91b957afde6e24c2991099b28bd2b97c"
+  license "Apache-2.0"
+
+  depends_on "gcc"
+
+  def install
+    ln_s "make.inc.macosx.gnu", "make.inc" if OS.mac?
+
+    # Don't use -march=native when building a bottle
+    inreplace "makefile", "-march=native", "-march=#{Hardware.oldest_cpu}" if build.bottle?
+
+    # Build libraries
+    system "make", "lib"
+
+    # Install libraries
+    lib.install "lib/libfmm3d.so"
+    lib.install "lib-static/libfmm3d.a"
+  end
+
+  test do
+    system "true"
+  end
+end
