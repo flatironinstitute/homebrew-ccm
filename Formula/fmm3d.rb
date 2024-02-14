8b63a5ee0f942e02257be798d418d02f7991455e
Dan Fortunato <dan.fortunato@gmail.com>
2024-02-14 21:24:59 +0000

    fmm3d: add 1.0.2 bottle.

--- Formula/fmm3d.rb
+++ Formula/fmm3d.rb
@@ -5,6 +5,12 @@ class Fmm3d < Formula
   sha256 "e29a1ebe485d2431d4e330334b2853bc91b957afde6e24c2991099b28bd2b97c"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://ghcr.io/v2/flatironinstitute/ccm"
+    sha256 cellar: :any,                 ventura:      "b656b343a56284d8a24d07850515ab99a45a6acd03ae0b0de4867d1fd745fd4b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "43b26f7877439572dccf5a17dc9c0b65e209552c281f2be31c021537b030dd75"
+  end
+
   depends_on "gcc"
 
   def install
