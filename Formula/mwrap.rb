2333b1467734a1b5ea96c88f85c2db6aeb703f41
Dan Fortunato <dan.fortunato@gmail.com>
2024-02-14 21:54:46 +0000

    mwrap: add 1.1.1 bottle.

--- Formula/mwrap.rb
+++ Formula/mwrap.rb
@@ -6,6 +6,12 @@ class Mwrap < Formula
   license "MIT"
   head "https://github.com/zgimbutas/mwrap.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/flatironinstitute/ccm"
+    sha256 cellar: :any_skip_relocation, ventura:      "22113a7b460fad9d128608ae736c3ddb94c737c4256a1d2193b019caf0121b1c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "80aeb61d13865f89cc047106d422ca9bfb562a93ed69094e45ebd7569bc9f37a"
+  end
+
   uses_from_macos "bison" => :build
   uses_from_macos "flex" => :build
 
