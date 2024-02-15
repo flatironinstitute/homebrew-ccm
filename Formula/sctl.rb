cd516f399642846613f5d10fc7cce99978d0601a
Dan Fortunato <dan.fortunato@gmail.com>
2024-02-15 03:06:05 +0000

    sctl: add 1.0.0 bottle.

--- Formula/sctl.rb
+++ Formula/sctl.rb
@@ -6,6 +6,12 @@ class Sctl < Formula
   license "Apache-2.0"
   head "https://github.com/dmalhotra/SCTL.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/flatironinstitute/ccm"
+    sha256 cellar: :any_skip_relocation, ventura:      "c1c119b41dd3415da9f99f74fb8e27e3e223ef902f0aa7c9130077bd0cd0dc41"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "abd0a8ddad6048889faf14f66e74bca423eb3f53df90760f4a4a1574daa158ac"
+  end
+
   def install
     include.install "include/sctl"
     include.install "include/sctl.hpp"
